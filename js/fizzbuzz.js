90df78b49aefd986b6027a621c0c335e68eb1ec9
David Bess <davidbess@Davids-MacBook-Pro.local>
2020-07-06 12:02:17 -0500

    jsone

---
+++ js/fizzbuzz.js
@@ -0,0 +1,30 @@
+function multipleOf(num, mod) {
+    return num % mod === 0;
+}
+
+function fizzBuzz() {
+    var output = "";
+    for(var i=1;i<=100;i++) {
+        switch(true) {
+            case multipleOf(i, 15) :
+                output += "FizzBuzz \n";
+                break;
+            case multipleOf(i, 3) :
+                output += "Fizz \n";
+                break;
+            case multipleOf(i, 5) :
+                output += "Buzz \n";
+                break;
+            default :
+                output += i + " \n";
+                break;
+        }
+    }
+    return output;
+}
+
+console.log(fizzBuzz());
+
+
+
+
